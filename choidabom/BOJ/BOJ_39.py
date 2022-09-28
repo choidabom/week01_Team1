d73f89d00335d057a341bad46cc9f0d46564323b
DaeBum <48302257+choidabom@users.noreply.github.com>
2022-09-28 21:02:45 +0900

    39 풀었다...!

--- choidabom/BOJ/BOJ_39.py
+++ choidabom/BOJ/BOJ_39.py
@@ -1,18 +1,41 @@
 # 10971: 외판원 순회 2
 # 각 도시간에 이동하는데 드는 비용은 행렬 W[i][j]형태
-# W[i][j]는 도시 i에서 도시 j로 가기 위한 비용
 
 import sys
 from itertools import permutations
-
 sys.stdin = open('choidabom/BOJ/input.txt', 'r')
 N = int(sys.stdin.readline()) # 도시의 수 N
 
+# 도시의 수만큼 도시 리스트로 나타내기
 course_list = []
 for i in range(N):
-    if i == 0:
-        course_list.append(i)
-print(course_list)
+    course_list.append(i)
 
-A = list(sys.stdin.readline().split() for i in range(N))
-print(A)
+# 도시의 수만큼 갈 수 있는 경로 순열로 나타내기
+cases = permutations(course_list, N)
+# print(cases)
+
+# W[i][j]는 도시 i에서 도시 j로 가기 위한 비용
+W = list(sys.stdin.readline().split() for i in range(N))
+# print(W)
+
+min_cost = 10000000
+
+for case in cases:
+    cost = 0
+    check = True
+    case = list(case)
+    # print(case)
+    for i in range(len(case)):
+        if int(W[case[i-1]][case[i]]) == 0:
+            check = False
+            break
+        else:
+            cost += int(W[case[i-1]][case[i]])
+    if check == True:
+        if min_cost > cost:
+            min_cost = cost
+    
+print(min_cost)
+
+
